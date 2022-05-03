a8131ff9531eac0b120fe31e9ae497e55c277e7f
kropidlowsky <kropidlowsky@protonmail.com>
2022-05-03 15:36:45 +0200

    Update settings.py

--- polish_parliament/polish_parliament/settings.py
+++ polish_parliament/polish_parliament/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'polish_parliament.spiders'
 #USER_AGENT = 'polish_parliament (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
